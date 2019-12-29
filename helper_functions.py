e545f203910fde6bdc66e272a74cb440d8927d1c
fiatveritas <jesse.gallegos@usc.edu>
2019-12-28 16:26:03 -0800

    feat: fixed labels to integer

--- helper_functions.py
+++ helper_functions.py
@@ -15,6 +15,21 @@ def generate_array(name):
 		holder = []
 	return new_data
 
+def generate_labels(name):
+	"""This method opens the passed file and returns an array filled with labels
+	from the file."""
+	with open(name, 'r') as training_data:
+		training_data = list(csv.reader(training_data, delimiter=';'))
+	training_data = [j.split(',') for i in training_data for j in i]
+	new_data = []
+	holder = []
+	for i in training_data:
+		for j in i:
+			holder.append(int(j))
+		new_data.append(holder)
+		holder = []
+	return new_data
+
 def collapse(list_1, list_2):
 	"""This method takes in two lists and puts the two lists in one."""
 	master_list = []
